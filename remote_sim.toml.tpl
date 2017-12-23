ac3c4916cf3618d33f8f4e95f9dd446c6f834b52
Leo Schwarz <mail@leoschwarz.com>
2017-12-23 14:58:07 +0100

    Add config from the networking example.

---
+++ remote_sim.toml.tpl
@@ -0,0 +1,8 @@
+[user]
+first_name = "Rust"
+last_name = "Amazing"
+password_plain = "1234"
+
+[sim]
+loginuri = "http://127.0.0.1:9000"
+
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // will be added.
 
 // TODO: Remove at some later time.
-#![allow(dead_code,unused_imports,unused_variables)]
+#![allow(dead_code, unused_imports, unused_variables)]
 
 extern crate alga;
 extern crate futures;
@@ -13,14 +13,19 @@ extern crate glium;
 extern crate lazy_static;
 extern crate nalgebra;
 extern crate opensim_networking;
+#[macro_use]
+extern crate serde_derive;
 extern crate tokio_core;
+extern crate toml;
 extern crate typenum;
 
+pub mod config;
 pub mod data;
-
 pub mod networking;
 pub mod render;
 
 fn main() {
+    let cfg = config::get_config("remote_sim.toml").expect("no config");
+
     render::render_world();
 }
