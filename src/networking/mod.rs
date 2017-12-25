12a7ee2799567ef08520cbb638fea54a78d174ae
Leo Schwarz <mail@leoschwarz.com>
2017-12-25 14:51:46 +0100

    A cleaner implementation of RegionConnection handling.

--- src/networking/mod.rs
+++ src/networking/mod.rs
@@ -15,71 +15,117 @@ use opensim_networking::logging::Log;
 use opensim_networking::simulator::{ConnectInfo, MessageHandlers, Simulator};
 use std::collections::HashMap;
 use std::thread::{self, JoinHandle};
+use std::sync::Arc;
+use std::hash::{Hash, Hasher};
 use tokio_core::reactor::Core;
 
 pub mod region_connection;
-use self::region_connection::{RegionConnection, RegionConnectionInternal};
+use self::region_connection::{EventRecv, RegionConnection, RegionConnectionInternal};
+
+#[derive(Clone, Debug)]
+pub struct RegionId {
+    /// The unique id in the networking struct.
+    id: u32,
+
+    /// ConnectInfo, only here for informational purposes, not used in
+    /// comparison.
+    connect_info: Arc<ConnectInfo>,
+}
+
+impl Hash for RegionId {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
+
+impl PartialEq for RegionId {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl Eq for RegionId {}
+
+struct Connection {
+    /// The communicator.
+    comm: RegionConnectionInternal,
+
+    /// The simulator connection.
+    sim: Simulator,
+}
 
 pub struct Networking {
-    thread_handle: JoinHandle<()>,
-    connect_tx: mpsc::Sender<(RegionConnectionInternal, ConnectInfo)>,
+    /// The thread where the networking code runs.
+    net_thread: JoinHandle<()>,
     log: Log,
+
+    /// Used to register a RegionConnection in the networking thread.
+    setup_conn: mpsc::Sender<(RegionConnectionInternal, RegionId)>,
 }
 
 impl Networking {
     pub fn new(log: Log) -> Self {
-        let (connect_tx, connect_rx) = mpsc::channel(1);
+        let (setup_conn_tx, setup_conn_rx) = mpsc::channel(1);
         let log_copy = log.clone();
 
-        // TODO: Implement this later, I'm outlining how to resolve the current mess lol.
-        //
-        // Channels:
-        // - setup_connection: (RegionId, mpsc::Sender<EventRecv>) client → net thread
-        // - outputs: HashMap<RegionId, mpsc::Sender<EventRecv> used for → client communication.
-        //
-        // There is only one channel with incoming events and tags to which region they are
-        // meant to, so only the sender is cloned for each new RegionConnection.
-        // But for each RegionConnection store in this thread a sender received on setting up
-        // the connection.
         let thread_handle = thread::spawn(move || {
-            let mut connections = Vec::new();
             let mut core = Core::new().unwrap();
-
-            let mut request_handlers = FuturesUnordered::new();
-
+            let mut conns: HashMap<RegionId, Connection> = HashMap::new();
 
             // TODO: probably need to use and_then and map_err here in the future.
             let handle = core.handle();
-            let connect_handler = connect_rx.map(|conn| {
-                let (conn_internal, conn_info) = conn;
-                let handlers = MessageHandlers::default();
-                let sim = Simulator::connect(&conn_info,
-                                             handlers,
-                                             handle.clone(),
-                                             &log_copy);
+            let setup_conn_handler = setup_conn_rx.map_err(|_| "");
+            let setup_conn_handler = setup_conn_handler.and_then(|tuple| {
+                // TODO: Why are type annotations required here?
+                let (conn_internal, region_id): (
+                    RegionConnectionInternal,
+                    RegionId,
+                ) = tuple;
 
-                connections.push(conn_internal);
-
-                // TODO
+                // TODO: This is blocking currently.
+                let sim_result = Simulator::connect(
+                    region_id.connect_info.as_ref(),
+                    MessageHandlers::default(),
+                    handle.clone(),
+                    &log_copy,
+                );
+                let send = if let Ok(sim) = sim_result {
+                    conns.insert(
+                        region_id,
+                        Connection {
+                            comm: conn_internal.clone(),
+                            sim: sim,
+                        },
+                    );
+                    conn_internal
+                        .send
+                        .clone()
+                        .send(EventRecv::ConnectResult(Ok(())))
+                } else {
+                    conn_internal
+                        .send
+                        .clone()
+                        .send(EventRecv::ConnectResult(Err(())))
+                };
+                send.map_err(|_| "MPMC send error.")
             });
-            let connect_handler = connect_handler.map_err(|_| "");
 
-            core.run(connect_handler.into_future());
+            core.run(setup_conn_handler.into_future());
         });
 
         Networking {
-            thread_handle: thread_handle,
-            connect_tx: connect_tx,
+            net_thread: thread_handle,
             log: log,
+            setup_conn: setup_conn_tx,
         }
     }
 
-    pub fn connect_region(&self, c_info: ConnectInfo) -> RegionConnection {
+    pub fn connect_region(&self, region_id: RegionId) -> RegionConnection {
         let (conn, conn_internal) = region_connection::new_pair();
         // TODO: Consider whether we really want to unwrap here?
-        self.connect_tx
+        self.setup_conn
             .clone()
-            .send((conn_internal, c_info))
+            .send((conn_internal, region_id))
             .wait()
             .unwrap();
         conn
--- src/networking/region_connection.rs
+++ src/networking/region_connection.rs
@@ -4,7 +4,7 @@ use multiqueue::{self, MPMCFutReceiver, MPMCFutSender};
 use std::sync::mpsc::RecvError;
 
 /// Managing the connection in the client code.
-//#[derive(Clone)]
+#[derive(Clone)]
 pub struct RegionConnection {
     recv: MPMCFutReceiver<EventRecv>,
     send: MPMCFutSender<EventSend>,
@@ -12,9 +12,10 @@ pub struct RegionConnection {
 
 /// The internal manager of the connection in the networking code (in the
 /// networking thread).
+#[derive(Clone)]
 pub struct RegionConnectionInternal {
-    recv: MPMCFutReceiver<EventSend>,
-    send: MPMCFutSender<EventRecv>,
+    pub recv: MPMCFutReceiver<EventSend>,
+    pub send: MPMCFutSender<EventRecv>,
 }
 
 pub fn new_pair() -> (RegionConnection, RegionConnectionInternal) {
@@ -35,11 +36,6 @@ pub fn new_pair() -> (RegionConnection, RegionConnectionInternal) {
 impl RegionConnection {
     pub fn send(&self, event: EventSend) {
         self.send.clone().send(event).map(|_| ()).wait();
-        // TODO: The above line should not require the EventSend to be Clone,
-        //       but the only alternative we have is try_send which can fail
-        //       if the buffer is full and will require us to implement a waiting
-        //       strategy here. → Ideally we can formulate this somehow using
-        //       a Future and .wait()
     }
 
     pub fn recv(&self) -> Result<EventRecv, RecvError> {
@@ -49,7 +45,7 @@ impl RegionConnection {
 
 pub enum EventRecv {
     TerrainPatch(TerrainPatch),
+    ConnectResult(Result<(), ()>),
 }
 
-#[derive(Clone)]
 pub enum EventSend {}
